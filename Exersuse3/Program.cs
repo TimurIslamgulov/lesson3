fde9646a48cce305e217846d698b910c2d90cd2a
Dmitry <ya.potapov@yandex.ru>
2022-07-14 18:27:20 +0300

    Task 15

---
+++ Exersuse3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Задача 15 ");
+
+Console.Write("Put dayNumber: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("(FreeDay) -> Yes");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("Not WeekDay");
+  }
+  else Console.WriteLine("( Not FreeDay) -> No");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
