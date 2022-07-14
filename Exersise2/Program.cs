7a2a084e28d2d99d92662a9416634076cc83ec40
Dmitry <ya.potapov@yandex.ru>
2022-07-14 18:16:16 +0300

    Task 13

---
+++ Exersise2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Task 13 ");
+
+Console.Write("Put Number: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2){
+  Console.WriteLine("ThirdDigit -> " + anyNumberText[2]);
+}
+else {
+  Console.WriteLine(" NoThirdDigit-> ");
+}
